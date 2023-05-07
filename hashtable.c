d2581ff75c3981c55f8d9e9b85e0c61c10d6b709
David Sha <david.sha2002@gmail.com>
2023-05-07 16:27:52 +1000

    feat: create hash table implementation

---
+++ hashtable.c
@@ -0,0 +1,89 @@
+/* =============================================================================
+   hashtable.c
+
+   Hash table implementation that allows any data type to be stored.
+
+   Author: David Sha
+============================================================================= */
+#include "hashtable.h"
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned long hash(unsigned char *str) {
+    unsigned long hash = 5381;
+    int c;
+    while ((c = *str++)) {
+        hash = ((hash << 5) + hash) + c;
+    }
+    return hash;
+}
+
+hashtable_t *hashtable_create(int size) {
+    assert(size > 0);
+    hashtable_t *new = (hashtable_t *)malloc(sizeof(*new));
+    assert(new);
+    new->table = (item_t **)calloc(size, sizeof(item_t *));
+    assert(new->table);
+    new->size = size;
+    new->count = 0;
+    return new;
+}
+
+void hashtable_destroy(hashtable_t *hashtable, void (*free_data)(void *data)) {
+    assert(hashtable);
+    for (int i = 0; i < hashtable->size; i++) {
+        if (hashtable->table[i]) {
+            free_list(hashtable->table[i]->data, free_data);
+            free(hashtable->table[i]);
+        }
+    }
+    free(hashtable->table);
+    free(hashtable);
+}
+
+void hashtable_insert(hashtable_t *hashtable, unsigned long key, void *data) {
+    assert(hashtable);
+    int index = key % hashtable->size;
+    if (hashtable->table[index]) {
+        hashtable->table[index]->data = prepend(hashtable->table[index]->data, data);
+    } else {
+        item_t *new = (item_t *)malloc(sizeof(*new));
+        assert(new);
+        new->key = key;
+        new->data = create_empty_list();
+        new->data = prepend(new->data, data);
+        hashtable->table[index] = new;
+        hashtable->count++;
+    }
+}
+
+item_t *hashtable_search(hashtable_t *hashtable, unsigned long key) {
+    assert(hashtable);
+    int index = key % hashtable->size;
+    if (hashtable->table[index]) {
+        return hashtable->table[index];
+    }
+    return NULL;
+}
+
+void hashtable_remove(hashtable_t *hashtable, unsigned long key, void (*free_data)(void *)) {
+    assert(hashtable);
+    int index = key % hashtable->size;
+    if (hashtable->table[index]) {
+        free_list(hashtable->table[index]->data, free_data);
+        free(hashtable->table[index]);
+        hashtable->table[index] = NULL;
+        hashtable->count--;
+    }
+}
+
+void hashtable_print(hashtable_t *hashtable, void (*print_data)(void *)) {
+    assert(hashtable);
+    for (int i = 0; i < hashtable->size; i++) {
+        if (hashtable->table[i]) {
+            printf("%d: ", i);
+            print_list(hashtable->table[i]->data, print_data);
+        }
+    }
+}
---
+++ hashtable.h
@@ -0,0 +1,96 @@
+/* =============================================================================
+   hashtable.h
+
+   Hash table implementation that allows any data type to be stored.
+
+   Reference:
+   - Inspired by DigitalOcean's implementation of a hash table in C++.
+     https://www.digitalocean.com/community/tutorials/hash-table-in-c-plus-plus
+
+   Author: David Sha
+============================================================================= */
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
+
+#include "linkedlist.h"
+
+/* structures =============================================================== */
+typedef struct item {
+    unsigned long key;
+    list_t *data;
+} item_t;
+
+typedef struct hashtable {
+    item_t **table;
+    int size;
+    int count;
+} hashtable_t;
+
+
+/* function prototypes ====================================================== */
+
+/*
+ * Hash function which is djb2 by Dan Bernstein.
+ * 
+ * @param str The string to hash.
+ * @return The hash value.
+ * @note See more on djb2 at https://theartincode.stanis.me/008-djb2/
+ */
+unsigned long hash(unsigned char *str);
+
+/*
+ * Creates a new hashtable.
+ * 
+ * @param size The size of the hashtable.
+ * @return A pointer to the hashtable, or NULL on failure.
+ */
+hashtable_t *hashtable_create(int size);
+
+/*
+ * Destroys a hashtable.
+ * 
+ * @param hashtable The hashtable to destroy.
+ * @param free_data The function to free the data of each item. NULL if no
+ * freeing is required.
+ */
+void hashtable_destroy(hashtable_t *hashtable, void (*free_data)(void *data));
+
+/*
+ * Insert an item to the hashtable. Count is incremented if the item is new.
+ * If the item already exists, the data is prepended to the list of data.
+ * 
+ * @param hashtable The hashtable to insert the item to.
+ * @param key The key of the item.
+ * @param data The data of the item.
+ */
+void hashtable_insert(hashtable_t *hashtable, unsigned long key, void *data);
+
+/*
+ * Search for an item in the hashtable.
+ * 
+ * @param hashtable The hashtable to search.
+ * @param key The key of the item.
+ * @return A pointer to the item, or NULL if not found.
+ */
+item_t *hashtable_search(hashtable_t *hashtable, unsigned long key);
+
+/*
+ * Remove an item from the hashtable. Count is decremented if the item is
+ * removed. If the item has multiple data, all data is removed.
+ * 
+ * @param hashtable The hashtable to remove the item from.
+ * @param key The key of the item.
+ * @param free_data The function to free the data of the item. NULL if no
+ * freeing is required.
+ */
+void hashtable_remove(hashtable_t *hashtable, unsigned long key, void (*free_data)(void *));
+
+/*
+ * Prints the hashtable.
+ * 
+ * @param hashtable The hashtable to print.
+ */
+void hashtable_print(hashtable_t *hashtable, void (*print_data)(void *));
+
+
+#endif
