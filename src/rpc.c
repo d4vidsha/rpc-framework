61df789b9c98e561f127b08bb57d53a73847711e
David Sha <dasha@student.unimelb.edu.au>
2023-05-18 14:19:08 +1000

    fix: remove one condition for is_malformed()

--- src/rpc.c
+++ src/rpc.c
@@ -565,8 +565,5 @@ int is_malformed(rpc_data *data) {
     if (data->data2 == NULL && data->data2_len != 0) {
         return TRUE;
     }
-    if (data->data2 != NULL && data->data2_len == 0) {
-        return TRUE;
-    }
     return FALSE;
 }
