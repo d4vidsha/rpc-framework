c4c672162a8b115a1bb3d4a37bdfb12c680f00ec
David Sha <dasha@student.unimelb.edu.au>
2023-05-18 10:34:15 +1000

    chore: add comments to header file

--- includes/linkedlist.h
+++ includes/linkedlist.h
@@ -31,22 +31,145 @@ typedef struct list {
 } list_t;
 
 /* function prototypes ====================================================== */
+
+/* 
+ * Creates an empty linked list.
+ * 
+ * @return a pointer to the linked list
+ */
 list_t *create_empty_list();
+
+/*
+ * Create a linked list given the head and foot.
+ * 
+ * @param head: the head of the linked list
+ * @param foot: the foot of the linked list
+ * @return a pointer to the linked list
+ */
 list_t *create_list(node_t *head, node_t *foot);
+
+/*
+ * Checks if list is empty.
+ *
+ * @param list: the linked list
+ */
 int is_empty_list(list_t *list);
+
+/*
+ * Free the list by freeing all nodes and its contents.
+ * Give a function pointer to free the data. If no function
+ * pointer is given, the data will not be freed.
+ * 
+ * @param list: the linked list
+ * @param free_data: function pointer to free the data
+ */
 void free_list(list_t *list, void (*free_data)(void *data));
+
+/*
+ * Prepend to the list i.e. add to head of linked list.
+ *
+ * @param list: the linked list
+ * @param data: the data to be added
+ */
 list_t *prepend(list_t *list, void *data);
+
+/*
+ * Append to the list i.e. add to foot of linked list.
+ * 
+ * @param list: the linked list
+ * @param data: the data to be added
+ */
 list_t *append(list_t *list, void *data);
+
+/*
+ * Get the length of the linked list.
+ * 
+ * @param list: the linked list
+ */
 int list_len(list_t *list);
+
+/*
+ * Remove a node from the linked list.
+ *
+ * @param list: the linked list
+ * @param node: the node address to be removed
+ */
 void remove_node(list_t *list, node_t *node);
+
+/*
+ * Remove data from the linked list. If data is not found,
+ * return NULL.
+ * 
+ * @param list: the linked list
+ * @param data: the data to be removed
+ */
 void *remove_data(list_t *list, void *data);
+
+/*
+ * Move data from one linked list to another.
+ *
+ * @param data: the address of the data to be moved
+ * @param from: the linked list to move from
+ * @param to: the linked list to move to
+ */
 void *move_data(void *data, list_t *from, list_t *to);
+
+/*
+ * Pop the head of the linked list.
+ *
+ * @param list: the linked list
+ */
 void *pop(list_t *list);
+
+/*
+ * Print the linked list in the format [data1, data2, ...].
+ *
+ * @param list: the linked list
+ * @param print_data: function pointer to print the data
+ */
 void print_list(list_t *list, void (*print_data)(void *));
+
+/*
+ * Insert data before a node.
+ *
+ * @param list: the linked list
+ * @param node: the node to insert before
+ * @param data: the data to be inserted
+ */
 list_t *insert_prev(list_t *list, node_t *node, void *data);
+
+/*
+ * Insert data after a node.
+ *
+ * @param list: the linked list
+ * @param node: the node to insert after
+ * @param data: the data to be inserted
+ */
 list_t *insert_next(list_t *list, node_t *node, void *data);
+
+/*
+ * Find data in the list. If data is not in the list, return NULL.
+ * 
+ * @param list: the linked list
+ * @param data: the data to be found
+ * @param cmp: function pointer to compare the data
+ * @return the address of the data
+ */
 void *find_node(list_t *list, void *data, int (*cmp)(void *, void *));
+
+/*
+ * Compare two addresses.
+ * 
+ * @param a: the first address
+ * @param b: the second address
+ * @return 0 if a == b, 1 otherwise
+ */
 int cmp_addr(void *a, void *b);
+
+/*
+ * Copy the data from one list to another. The data is not copied, only
+ * the pointers to the data are copied.
+ */
 void copy_list(list_t *from, list_t *to);
 
 #endif
--- src/linkedlist.c
+++ src/linkedlist.c
@@ -12,8 +12,6 @@
 #include <string.h>
 
 list_t *create_empty_list() {
-    /*  Creates an empty linked list.
-     */
     list_t *list;
     list = (list_t *)malloc(sizeof(*list));
     assert(list);
@@ -22,8 +20,6 @@ list_t *create_empty_list() {
 }
 
 list_t *create_list(node_t *head, node_t *foot) {
-    /*  Create a linked list given the head and foot.
-     */
     assert(head && foot);
     list_t *new = create_empty_list();
     new->head = head;
@@ -32,17 +28,11 @@ list_t *create_list(node_t *head, node_t *foot) {
 }
 
 int is_empty_list(list_t *list) {
-    /*  Checks if list is empty.
-     */
     assert(list);
     return list->head == NULL;
 }
 
 void free_list(list_t *list, void (*free_data)(void *data)) {
-    /*  Free the list by freeing all nodes and its contents.
-        Give a function pointer to free the data. If no function
-        pointer is given, the data will not be freed.
-     */
     assert(list);
     node_t *curr, *prev;
     curr = list->head;
@@ -58,8 +48,6 @@ void free_list(list_t *list, void (*free_data)(void *data)) {
 }
 
 list_t *prepend(list_t *list, void *data) {
-    /*  Prepend to the list i.e. add to head of linked list.
-     */
     assert(list && data);
     node_t *new;
     new = (node_t *)malloc(sizeof(*new));
@@ -70,7 +58,7 @@ list_t *prepend(list_t *list, void *data) {
     if (list->head) {
         list->head->prev = new;
     } else {
-        /* this is the first insert into list */
+        // this is the first insert into list
         list->foot = new;
     }
     list->head = new;
@@ -78,8 +66,6 @@ list_t *prepend(list_t *list, void *data) {
 }
 
 list_t *append(list_t *list, void *data) {
-    /*  Append to the list i.e. add to foot of linked list.
-     */
     assert(list && data);
     node_t *new;
     new = (node_t *)malloc(sizeof(*new));
@@ -90,7 +76,7 @@ list_t *append(list_t *list, void *data) {
     if (list->foot) {
         list->foot->next = new;
     } else {
-        /* this is the first insert into list */
+        // this is the first insert into list
         list->head = new;
     }
     list->foot = new;
@@ -98,8 +84,6 @@ list_t *append(list_t *list, void *data) {
 }
 
 int list_len(list_t *list) {
-    /*  Get the linked list length.
-     */
     assert(list);
     int len = 0;
     node_t *curr;
@@ -112,27 +96,25 @@ int list_len(list_t *list) {
 }
 
 void remove_node(list_t *list, node_t *node) {
-    /*  Remove a node from the list.
-     */
     assert(list && node);
     if (node->prev == NULL) {
-        /* removing the first node in the list */
+        // removing the first node in the list
         list->head = node->next;
         if (list->foot == node) {
-            /* also removing the last node in the list */
+            // also removing the last node in the list
             list->foot = NULL;
         } else {
-            /* set the prev pointer of the new head */
+            // set the prev pointer of the new head
             list->head->prev = NULL;
         }
     } else {
-        /* removing a node in the middle or end of the list */
+        // removing a node in the middle or end of the list
         node->prev->next = node->next;
         if (list->foot == node) {
-            /* also removing the last node in the list */
+            // also removing the last node in the list
             list->foot = node->prev;
         } else {
-            /* set the prev pointer of the next node */
+            // set the prev pointer of the next node
             node->next->prev = node->prev;
         }
     }
@@ -140,8 +122,6 @@ void remove_node(list_t *list, node_t *node) {
 }
 
 void *remove_data(list_t *list, void *data) {
-    /*  Remove data from the list. If data is not in the list, return NULL.
-     */
     assert(list && data);
     node_t *curr;
     curr = list->head;
@@ -156,8 +136,6 @@ void *remove_data(list_t *list, void *data) {
 }
 
 void *move_data(void *data, list_t *from, list_t *to) {
-    /*  Move data from one list to another.
-     */
     assert(data && from && to);
     void *removed = remove_data(from, data);
     if (removed) {
@@ -167,17 +145,15 @@ void *move_data(void *data, list_t *from, list_t *to) {
 }
 
 void *pop(list_t *list) {
-    /*  Pop from the head of list.
-     */
     assert(list);
     node_t *head = list->head;
     if (head) {
         list->head = head->next;
         if (list->foot == head) {
-            /* also removing the last node in the list */
+            // also removing the last node in the list
             list->foot = NULL;
         } else {
-            /* set the prev pointer of the new head */
+            // set the prev pointer of the new head
             list->head->prev = NULL;
         }
         void *data = head->data;
@@ -188,8 +164,6 @@ void *pop(list_t *list) {
 }
 
 void print_list(list_t *list, void (*print_data)(void *)) {
-    /*  Print a linked list in the format [data1, data2, ...].
-     */
     assert(list);
     printf("[");
     for (node_t *curr = list->head; curr; curr = curr->next) {
@@ -202,8 +176,6 @@ void print_list(list_t *list, void (*print_data)(void *)) {
 }
 
 list_t *insert_prev(list_t *list, node_t *node, void *data) {
-    /*  Insert data before the given node.
-     */
     assert(list && node && data);
     node_t *new;
     new = (node_t *)malloc(sizeof(*new));
@@ -214,7 +186,7 @@ list_t *insert_prev(list_t *list, node_t *node, void *data) {
     if (node->prev) {
         node->prev->next = new;
     } else {
-        /* this is the first insert into list */
+        // this is the first insert into list
         list->head = new;
     }
     node->prev = new;
@@ -222,8 +194,6 @@ list_t *insert_prev(list_t *list, node_t *node, void *data) {
 }
 
 list_t *insert_next(list_t *list, node_t *node, void *data) {
-    /*  Insert data after the given node.
-     */
     assert(list && node && data);
     node_t *new;
     new = (node_t *)malloc(sizeof(*new));
@@ -234,7 +204,7 @@ list_t *insert_next(list_t *list, node_t *node, void *data) {
     if (node->next) {
         node->next->prev = new;
     } else {
-        /* this is the first insert into list */
+        // this is the first insert into list
         list->foot = new;
     }
     node->next = new;
@@ -242,8 +212,6 @@ list_t *insert_next(list_t *list, node_t *node, void *data) {
 }
 
 void *find_node(list_t *list, void *data, int (*cmp)(void *, void *)) {
-    /*  Find data in the list. If data is not in the list, return NULL.
-     */
     assert(list && data && cmp);
     for (node_t *curr = list->head; curr; curr = curr->next) {
         if (cmp(curr->data, data) == 0) {
@@ -254,15 +222,10 @@ void *find_node(list_t *list, void *data, int (*cmp)(void *, void *)) {
 }
 
 int cmp_addr(void *a, void *b) {
-    /*  Compare two addresses. If a == b, return 0. Otherwise, return 1.
-     */
     return a != b;
 }
 
 void copy_list(list_t *from, list_t *to) {
-    /*  Copy the data from one list to another. The data is not copied, only
-        the pointers to the data are copied.
-     */
     for (node_t *curr = from->head; curr; curr = curr->next) {
         void *data = curr->data;
         append(to, data);
